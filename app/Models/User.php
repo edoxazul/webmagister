b17668ab33f510f671db6634de534db30425c253
Eduardo <edualvsald@hotmail.com>
2020-11-12 09:01:58 -0300

    feat (magisterweb) : primer commit

---
+++ app/Models/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The database connection used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mysqlLogin';
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'rut',
+        'rol',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        // 'two_factor_recovery_codes',
+        // 'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+    <?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+           'name' => 'Jose Pena',
+            'rut' => '18128077-8',
+            'rol' => 'Administrador',
+            'email' => 'jose@email.com',
+            'password' => bcrypt('123')
+        ]);
+
+        App\User::create([
+            'name' => 'Maikel Carvajal',
+            'rut' => '19397790-1',
+            'rol' => 'Profesor',
+            'email' => 'maikel@email.com',
+            'password' => bcrypt('321')
+        ]);
+
+        App\User::create([
+            'name' => 'Nicole Perez',
+            'rut' => '87654321',
+            'rol' => 'Secretaria',
+            'email' => 'nicole@email.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
