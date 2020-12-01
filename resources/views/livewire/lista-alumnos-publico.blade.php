fb07509a8957a2cb387d4d0f922952c84fca86fb
Javier <javier.burgos@alumnos.ucn.cl>
2020-12-01 20:52:34 -0300

    update: lista alumnos publica sin alumnos eliminados

--- resources/views/livewire/lista-alumnos-publico.blade.php
+++ resources/views/livewire/lista-alumnos-publico.blade.php
@@ -155,54 +155,7 @@
                     @endif
                 @endforeach
 
-                <h1 class="w-full">Alumnos Eliminados</h1>
 
-                @foreach ($alumnos as $alumno)
-
-
-                    @if ($alumno->estatus_alumno == 'Eliminado')
-
-                        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                                <a href="#">
-                                    <img alt="Placeholder" class="block w-full h-auto"
-                                        src="https://picsum.photos/600/400/?random">
-                                </a>
-
-                                <header class="flex items-center justify-between p-2 leading-tight md:p-4">
-                                    <h1 class="text-lg">
-                                        <a class="text-black no-underline hover:underline" href="#">
-                                            {{ $alumno->estatus_alumno }}
-                                        </a>
-                                    </h1>
-                                    <p class="text-sm text-grey-darker">
-
-                                    </p>
-                                </header>
-
-                                <footer class="flex items-center justify-between p-2 leading-none md:p-4">
-                                    <a class="flex items-center text-black no-underline hover:underline" href="#">
-                                        <img alt="Placeholder" class="block rounded-full"
-                                            src="https://picsum.photos/32/32/?random">
-                                        <p class="ml-2 text-sm">
-                                            {{ $alumno->nombre_alumno }}
-
-                                        </p>
-                                    </a>
-                                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                                        <span class="hidden">Like</span>
-                                        <i class="fa fa-heart"></i>
-                                    </a>
-                                </footer>
-                            </article>
-
-                            <!-- END Article -->
-
-                        </div>
-                        <!-- END Column -->
-                    @endif
-                @endforeach
 
 
             @endif
