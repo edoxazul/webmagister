9e1ece3b283fc0feb2310176e94f57f6a024c4ce
Bárbara <bhp001@alumnos.ucn.cl>
2020-12-01 22:48:58 -0300

    update: Vista pública info magister con datos por código
    No está ligado al panel de admin. Sino que directo al código. Para fines de visualización.

--- resources/views/livewire/info-magister-publico.blade.php
+++ resources/views/livewire/info-magister-publico.blade.php
@@ -2,16 +2,55 @@
     <div class="container px-4 mx-auto my-12 md:px-12">
 
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <h1 style="text-align: justify;">Información General</h1>
+            <h1 class="w-full">Información General</h1>
         </div>
-
-        <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <article class="overflow-hidden rounded-lg shadow-lg">
+            <p>
+            </p>
+            <p>
+            </p>
                 <h2 style="text-align: justify;">Propósito</h2>
                 <p>
-                <h3 style="text-align: justify;">LALALALALA</h3>
-            </article>
-        </div>
+                <h3 style="text-align: justify;">
+                    El Programa de Magíster en Ingeniería Informática está dirigido a todos aquellos licenciados y/o ingenieros de la especialidad y profesiones afines que tengan inquietudes de perfeccionamiento en el área, y cuyo propósito sea desarrollar investigación básica y/o aplicada. Es también una alternativa para los actuales alumnos de las carreras de Ingeniería de Ejecución y Civil en Computación e Informática de la UCN, que deseen obtener el grado de magíster.
+                </h3>
+
     </div>
+
+    <div class="container px-4 mx-auto my-12 md:px-12">
+                <h2 style="text-align: justify;">Objetivo</h2>
+                <p>
+                <h3 style="text-align: justify;">
+                    El objetivo del programa de Magíster en Ingeniería Informática es la formación de graduados de alto nivel,
+                    con conocimientos avanzados en Computación, capaces de impulsar y desarrollar
+                    investigación básica y aplicada, impartir docencia, prestar asesorías especializadas y
+                    resolver problemas reales y complejos de las empresas, muy en particular de las empresas regionales.</h3>
+
+    </div>
+
+    <div class="container px-4 mx-auto my-12 md:px-12">
+                <h2 style="text-align: justify;">Descripción</h2>
+                <p>
+                <h3 style="text-align: justify;">
+                    El Magíster en Ingeniería Informática ofrece una sólida formación en tres áreas del conocimiento: Ingeniería de Software, Ingeniería de Datos y del Conocimiento y Automatización y Robótica, todas ligadas directamente a las líneas de investigación definidas en la unidad.
+
+                    Entrega conocimientos y capacidades para el uso de herramientas con el fin de encontrar soluciones eficientes y efectivas a diferentes problemas asociados con el mejoramiento de los procesos de desarrollo de soluciones informáticas.
+
+                    El Programa cuenta con laboratorios especializados para apoyar la formación de los estudiantes entre ellos se encuentran: Laboratorio de Inteligencia Artificial, Laboratorio de Ingeniería de Software, Laboratorio de Ingeniería de Datos, Laboratorio de Tecnologías Emergentes, Laboratorio de Computación Aplicada, Laboratorio de Redes entre otros. Cada uno de ellos cuenta con el hardware y software requerido para el desarrollo de cada una de las asignaturas del programa.
+
+                </h3>
+    </div>
+
+    <div class="container px-4 mx-auto my-12 md:px-12">
+        <h2 style="text-align: justify;">Contacto</h2>
+        <p>
+        <h3 style="text-align: justify;">
+            Director: Claudio Meneses Villegas
+            <p>
+            Teléfono: (56-55) 2355158
+            <p>
+            Email: cmeneses@ucn.cl
+        </h3>
+</div>
+
 </div>
 
