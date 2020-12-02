4a6700121a3d96f170f1e8323cf73a33c1aba9f3
Eduardo <edualvsald@hotmail.com>
2020-12-02 20:39:50 -0300

    fix: reparado filtro academicos publico

--- resources/views/livewire/lista-academicos-publico.blade.php
+++ resources/views/livewire/lista-academicos-publico.blade.php
@@ -1,42 +1,37 @@
-<div class="flex w-full px-2 mx-auto bg-gray-300 rounded-md max-w-7xl sm:px-6 lg:px-8">
-    <div class="relative flex items-center justify-center w-full h-16">
-        <h2 class="justify-center w-full text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-            Cuerpo Academico
-        </h2>
-
-        <div class="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-        </div>
-    </div>
-    <div class="flex w-full p-5 md:w-1/3 md:text-left">
-        <div class="absolute flex sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <span class="flex sm:block">
-                <div
-                    class="inline-flex items-center justify-center px-4 py-2 ml-4 rounded-md shadow-sm lg:mt-0 lg:ml-4 form-input">
-                    <select wire:model='estatus' class="text-sm text-gray-700 outline-none">
-                        <option value="Claustro">Claustro</option>
-                        <option value="Colaborador">Colaborador</option>
-                        <option value="Visitante">Visitante</option>
-                    </select>
-                </div>
-            </span>
-
-            <span class="flex ml-3 sm:block">
-                <button wire:click="clear" type="button"
-                    class="inline-flex items-center justify-center px-4 py-2 ml-4 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <!-- Heroicon name: link -->
-                    X
-                </button>
-            </span>
-        </div>
-
-
-    </div>
-</div>
-
-
 <div>
 
+    <div class="flex w-full px-2 mx-auto bg-gray-300 rounded-md max-w-7xl sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-center w-full h-16">
+            <h2 class="justify-center w-full text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                Cuerpo Academico
+            </h2>
 
+            <div class="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+            </div>
+        </div>
+        <div class="flex w-full p-5 md:w-1/3 md:text-left">
+            <div class="absolute flex sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <span class="flex sm:block">
+                    <div
+                        class="inline-flex items-center justify-center px-4 py-2 ml-4 rounded-md shadow-sm lg:mt-0 lg:ml-4 form-input">
+                        <select wire:model='estatus' class="text-sm text-gray-700 outline-none">
+                            <option value="Claustro">Claustro</option>
+                            <option value="Colaborador">Colaborador</option>
+                            <option value="Visitante">Visitante</option>
+                        </select>
+                    </div>
+                </span>
+
+                <span class="flex ml-3 sm:block">
+                    <button wire:click="clear" type="button"
+                        class="inline-flex items-center justify-center px-4 py-2 ml-4 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <!-- Heroicon name: link -->
+                        X
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
 
     <div class="container px-4 mx-auto my-12 md:px-12">
 
@@ -193,16 +188,7 @@
                         <!-- END Column -->
                     @endif
                 @endforeach
-
-
             @endif
-
-
-
-
-
-
-
         </div>
     </div>
 
