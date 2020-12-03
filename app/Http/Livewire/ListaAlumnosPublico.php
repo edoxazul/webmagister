6ac71b7f91533e4d05702d9cf1a18faf678e40a3
Bárbara <bhp001@alumnos.ucn.cl>
2020-12-03 18:10:20 -0300

    fix: Replica barra título y filtro en info general y alumno

--- app/Http/Livewire/ListaAlumnosPublico.php
+++ app/Http/Livewire/ListaAlumnosPublico.php
@@ -14,10 +14,18 @@ class ListaAlumnosPublico extends Component
     public $page='1';
     public $nombre_alumno,$rut_alumno,$nombre_pregrado_alumno,$nombre_institucion_alumno,$contacto_alumno,$estatus_alumno,$anio_ingreso,$linkedin;
 
+    // public function render()
+    // {
+    //     return view('livewire.lista-alumnos-publico',[
+    //         'alumnos' => Alumnos::where('nombre_alumno','like','%' . trim($this->search) . '%')
+    //         ->paginate()
+    //     ])
+    //     ->layout('layouts.guest');
+    // }
     public function render()
     {
         return view('livewire.lista-alumnos-publico',[
-            'alumnos' => Alumnos::where('nombre_alumno','like','%' . trim($this->search) . '%')
+            'alumnos' => Alumnos::where('estatus_alumno','LIKE',"%{$this->estatus_alumno}%")
             ->paginate()
         ])
         ->layout('layouts.guest');
--- resources/views/livewire/info-magister-publico.blade.php
+++ resources/views/livewire/info-magister-publico.blade.php
@@ -1,14 +1,21 @@
 <div>
+
+    <div class="flex w-full px-2 mx-auto bg-gray-300 rounded-md max-w-7xl sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-center w-full h-16">
+            <h2 class="justify-center w-full text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                Información General
+            </h2>
+
+        </div>
+    </div>
+
     <div class="container px-4 mx-auto my-12 md:px-12">
 
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <h1 class="w-full">Información General</h1>
-        </div>
             <p>
             </p>
             <p>
             </p>
-                <h2 style="text-align: justify;">Propósito</h2>
+            <h1 class="w-full">Propósito</h1>
                 <p>
                 <h3 style="text-align: justify;">
                     El Programa de Magíster en Ingeniería Informática está dirigido a todos aquellos licenciados y/o ingenieros de la especialidad y profesiones afines que tengan inquietudes de perfeccionamiento en el área, y cuyo propósito sea desarrollar investigación básica y/o aplicada. Es también una alternativa para los actuales alumnos de las carreras de Ingeniería de Ejecución y Civil en Computación e Informática de la UCN, que deseen obtener el grado de magíster.
@@ -17,7 +24,7 @@
     </div>
 
     <div class="container px-4 mx-auto my-12 md:px-12">
-                <h2 style="text-align: justify;">Objetivo</h2>
+            <h1 class="w-full">Objetivo</h1>
                 <p>
                 <h3 style="text-align: justify;">
                     El objetivo del programa de Magíster en Ingeniería Informática es la formación de graduados de alto nivel,
@@ -28,7 +35,7 @@
     </div>
 
     <div class="container px-4 mx-auto my-12 md:px-12">
-                <h2 style="text-align: justify;">Descripción</h2>
+                <h1 class="w-full">Descripción</h1>
                 <p>
                 <h3 style="text-align: justify;">
                     El Magíster en Ingeniería Informática ofrece una sólida formación en tres áreas del conocimiento: Ingeniería de Software, Ingeniería de Datos y del Conocimiento y Automatización y Robótica, todas ligadas directamente a las líneas de investigación definidas en la unidad.
@@ -41,7 +48,7 @@
     </div>
 
     <div class="container px-4 mx-auto my-12 md:px-12">
-        <h2 style="text-align: justify;">Contacto</h2>
+        <h1 class="w-full">Contacto</h1>
         <p>
         <h3 style="text-align: justify;">
             Director: Claudio Meneses Villegas
