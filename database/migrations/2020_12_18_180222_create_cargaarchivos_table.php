29f52239fa9d32b5b912995d05667a6b14250cae
Bárbara <bhp001@alumnos.ucn.cl>
2020-12-18 18:12:03 -0300

    Tabla módulo archivos

---
+++ database/migrations/2020_12_18_180222_create_cargaarchivos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargaarchivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carga_archivos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_archivo');
+            $table->string('descripcion_archivo');
+            $table->string('enlace_archivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carga_archivos');
+    }
+}
