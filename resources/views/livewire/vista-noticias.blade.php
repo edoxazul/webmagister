cce1bf037b54c4abe392d0fa1f8e7a4f1da7cf74
Bárbara <bhp001@alumnos.ucn.cl>
2020-12-28 21:49:53 -0300

    fix: vista de cada noticia

--- resources/views/livewire/vista-noticias.blade.php
+++ resources/views/livewire/vista-noticias.blade.php
@@ -37,30 +37,53 @@
                         class="justify-center w-full text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
                         Noticias Magíster
                     </h2>
-                    {{-- {{ dd($this->modelId) }} --}}
-
-
-
 
                 </div>
 
-
-            </div>
-
-            <div class="flex w-full px-2 py-2 mx-auto bg-gray-300 rounded-md max-w-7xl sm:px-6 lg:px-8">
-            Título: {{ $noticia->titulo_noticia }}
-
-            </div>
-            <div class="flex w-full px-2 py-5 mx-auto bg-gray-300 rounded-md max-w-7xl sm:px-6 lg:px-8">
-                Cuerpo: {{ $noticia->descripcion_noticia }}
-
             </div>
             </main>
+
+            <div class="mt-2"> </div>
+
+            <div class="w-full mx-auto md:w-2/3">
+                {{-- <div class="mx-5 my-3 text-sm">
+                <a href="" class="font-bold tracking-widest text-red-600 ">CORONAVIRUS</a>
+                </div> --}}
+                <div class="w-full px-5 text-4xl font-bold leading-none text-gray-800">
+                    {{ $noticia->titulo_noticia }}
+                </div>
+
+                <div class="w-full px-5 pt-2 pb-5 text-gray-500">
+                    {{ $noticia->autor_noticia }} |  {{date("d/m/Y", strtotime($noticia->created_at))}}
+                </div>
+
+                <div class="mx-20">
+                <img src="https://static.politico.com/dims4/default/fcd6d6a/2147483647/resize/1920x/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2F22%2F87%2F2259ffd444678054896b9fa32b4d%2Fgettyimages-1221513169.jpg">
+                {{-- <img src="{{ $noticia->noticia_photo_path }}"> --}}
+                </div>
+
+                <div class="w-full mx-5 text-gray-600 text-normal">
+                    <p class="py-3 border-b"></p>
+                </div>
+
+                {{-- <div class="w-full px-5 pt-3 italic font-thin text-gray-600">
+                    By <strong class="text-gray-700">Quint Forgey</strong><br>
+                    07/17/2020 09:57 AM EDT<br>
+                    Updated: 07/17/2020 10:33 AM EDT
+                </div> --}}
+
+                <div class="w-full px-5 mx-auto" style="text-align: justify;">
+                    <p class="my-5"> {!!  nl2br(e($noticia->descripcion_noticia)) !!} </p>
+                </div>
+            </div>
+
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
+
     <x-footer/>
 
     </body>
