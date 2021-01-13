f01fb233479119968532e55c9dbbfff14cf16d5b
Eduardo <edualvsald@hotmail.com>
2021-01-13 11:53:17 -0300

    update: logo MII en hosting de imagenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,26 @@
 <x-guest-layout>
     {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot> --}}
 
     <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="relative bg-white overflow-hidden">
-    <div class="max-w-7xl mx-auto">
+<div class="relative overflow-hidden bg-white">
+    <div class="mx-auto max-w-7xl">
       <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-        <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+        <svg class="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
           <polygon points="50,0 100,0 50,100 0,100" />
         </svg>
 
-        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
           <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
             <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
               <div class="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span class="sr-only">Workflow</span>
                 </a>
-                <div class="-mr-2 flex items-center md:hidden">
+                <div class="flex items-center -mr-2 md:hidden">
 
                 </div>
               </div>
@@ -39,19 +39,20 @@
             To: "opacity-0 scale-95"
         -->
 
-        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <main class="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div class="sm:text-center lg:text-left">
             <div class="max-w-xl px-0 m-auto">
                 <div class="bg-white" >
                     <div>
-                        <img src={{asset('/storage/logo-MII.jpeg')}}>
+                        <img src="https://i.ibb.co/wpJPrb3/logo-MII.jpg" alt="logo-MII" border="0">
+                        {{-- <img src={{asset('/storage/logo-MII.jpeg')}}> --}}
                     </div>
                 </div>
             </div>
             <br>
-            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
+            <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-5xl">
               <span class="block xl:inline">Bienvenido al Sitio Web del</span>
-              <span class="block text-indigo-600 xl:inline">Magister en Ingeniería Informática</span>
+              <span class="block text-blue-900 xl:inline">Magister en Ingeniería Informática</span>
             </h1>
             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
               Toda la información relacionada al MII en un solo sitio.
@@ -61,7 +62,7 @@
       </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
+      <img class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
     </div>
   </div>
 </x-guest-layout>
