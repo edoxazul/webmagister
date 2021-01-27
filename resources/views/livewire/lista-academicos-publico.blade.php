20a3ed9e00807e22c91a51080f9b58205f289727
Eduardo <edualvsald@hotmail.com>
2021-01-27 16:10:50 -0300

    fix: corregido bug en lista académicos público

--- resources/views/livewire/lista-academicos-publico.blade.php
+++ resources/views/livewire/lista-academicos-publico.blade.php
@@ -310,7 +310,7 @@
                                     Estado:
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {{$academico->estatus}}
+                                    {{$estatus}}
                                 </dd>
                                 </div>
                             <div class="px-4 py-3 bg-gray-100 border border-gray-200 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
